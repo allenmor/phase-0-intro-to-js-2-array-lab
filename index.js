7124597943bc92bae53119d83d8475ec5519fb3c
Allen Mordukhaev <allenmor93@gmail.com>
2022-07-19 18:28:38 -0400

    commit tree 37701b360a6584598cb6da95e5c46cf25ddc97d1

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+const destructivelyAppendCat = function(a) {
+    return cats.push(a);
+}
+
+const destructivelyPrependCat = function(a) {
+    return cats.unshift(a);
+}
+
+const destructivelyRemoveLastCat = function(a) {
+    return cats.pop(a);
+}
+
+const destructivelyRemoveFirstCat = function(a) {
+    return cats.shift(a);
+}
+
+const appendCat = function(a) {
+    return [...cats, a]
+}
+
+const prependCat = function(a) {
+    return [a, ...cats];
+}
+
+const removeLastCat = function(a) {
+    return a = cats.slice(0, cats.length - 1);
+}
+
+const removeFirstCat = function() {
+    return cats.slice(1, cats.length)
+}
